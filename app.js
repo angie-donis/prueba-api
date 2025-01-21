dcc5ae6a83ec377ff32236f4e141ed8abdf18407
AngieDonis <angie2000donis@gmail.com>
2025-01-20 23:27:48 -0600

    Add comments to app.js for better code clarity

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express');// Importa el paquete express
+const bodyParser = require('body-parser');// Importa el paquete body-parser
 const cors = require('cors'); // Importa el middleware cors
-const app = express()
-const PORT = 8000
-const { v4: uuidv4 } = require('uuid');
+const app = express()// Crea una instancia de express
+const PORT = 8000// Puerto en el que escuchará el servidor
+const { v4: uuidv4 } = require('uuid');// Importa la función v4 del paquete uuid
 
-let companies = [
+let companies = [// Arreglo de empresas
   {
     id: crypto.randomUUID(),
     status: 'Activo',
@@ -56,33 +56,33 @@ app.use(bodyParser.json());
 // Middleware para permitir solicitudes CORS
 app.use(cors());
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {// Ruta raíz
   res.send('Companies api is running! 🚀')
 })
 
-app.get('/companies/:id', (req, res) => {
+app.get('/companies/:id', (req, res) => {// Ruta para obtener una empresa por id
   const id = req.params.id;
   const company = companies.find(c => c.id === id);
   res.json(company);
 })
 
-app.get('/companies', (req, res) => {
+app.get('/companies', (req, res) => {// Ruta para obtener todas las empresas
   res.json(companies);
 })
 
-app.post("/companies", (req, res) => {
+app.post("/companies", (req, res) => {// Ruta para crear una empresa
   const company = { ...req.body, id: uuidv4() };
   companies.push(company);
   res.json(company);
 })
 
-app.put('/companies', (req, res) => {
+app.put('/companies', (req, res) => {// Ruta para actualizar una empresa
   const company = req.body;
   companies = companies.map(c => c.id === company.id ? company : c)
   res.json(company);
 })
 
-app.delete('/companies/:id', (req, res) => {
+app.delete('/companies/:id', (req, res) => {// Ruta para eliminar una empresa
   const id = req.params?.id;
   companies = companies.filter(f => f.id !== id);
   res.json({
@@ -90,6 +90,6 @@ app.delete('/companies/:id', (req, res) => {
   })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, () => {// Inicia el servidor
   console.log(`✅ Server is running on port ${PORT}`);
 })
